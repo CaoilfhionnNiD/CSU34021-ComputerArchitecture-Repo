a86b131e0830ae4360d031b23bb61105d358f140
Caoilfhionn Doran <caoilfhionndoran@Caoilfhionns-MacBook-Air.local>
2025-12-18 22:23:17 +0000

    testing

--- run_test.sh
+++ run_test.sh
@@ -2,13 +2,13 @@
 set -e
 
 TOTAL_POINTS=0
-MAX_POINTS=10
+MAX_POINTS=35
 
 ./server &
 SERVER_PID=$!
 sleep 1  
 
-run_test() {
+run_create_user_tests() {
     TEST_NAME=$1
     CLIENT_CMD=$2 
     EXPECTED_OUTPUT=$3
@@ -44,8 +44,52 @@ run_test() {
     fi
 }
 
-run_test "Create User" "qemu-riscv64 ./client create anthony" "expected_client_output.txt" 5 anthony
-run_test "Create User" "qemu-riscv64 ./client create bob" "expected_client_output.txt" 5 bob
+run_add_friend_test() {
+    TEST_NAME=$1
+    CLIENT_CMD=$2 
+    EXPECTED_OUTPUT=$3
+    POINTS=$4
+    FOLDER=$5
+
+    echo "Running test: $TEST_NAME (worth $POINTS points)"
+
+    # Run client 
+    if ! eval "$CLIENT_CMD" >client.out 2>&1; then
+        echo "Client crashed"
+        kill $SERVER_PID
+        return 0
+    fi
+
+    # Compare output
+    if diff -u "$EXPECTED_OUTPUT" client.out; then
+        echo "Test passed! +$POINTS points"
+        TOTAL_POINTS=$((TOTAL_POINTS + $POINTS))
+    else
+        echo "Test failed!"
+    fi
+
+    if diff -u "$EXPECTED_OUTPUT" client.out; then
+        if [ -d "$FOLDER" ] && [ "$(find "$FOLDER" -maxdepth 1 -type f -name "*.txt" | wc -l)" -eq 2 ]; then
+            echo "Test passed! +$POINTS points"
+            TOTAL_POINTS=$((TOTAL_POINTS + POINTS))
+        else
+            echo "Test failed! Folder missing or does not contain exactly 2 .txt files"
+        fi
+    else
+        echo "Test failed! Client output mismatch"
+    fi
+}
+
+run_create_user_tests "Create User" "qemu-riscv64 ./client create anthony" "expected_client_output.txt" 5 anthony
+run_create_user_tests "Create User" "qemu-riscv64 ./client create bob" "expected_client_output.txt" 5 bob
+run_create_user_tests "Create User" "qemu-riscv64 ./client create bob" "expected_output_user_exists.txt" 5 bob
+run_create_user_tests "Create User" "qemu-riscv64 ./client create" "expected_output_no_id.txt" 5
+
+
+run_add_friend_test "Add Friend" "qemu-riscv64 ./client add anthony bob" "expected_output_ok.txt" 5
+run_add_friend_test "Add Friend" "qemu-riscv64 ./client add anthony bill" "expected_output_no_friend.txt" 5
+run_add_friend_test "Add Friend" "qemu-riscv64 ./client add bill bob" "expected_output_no_id2.txt" 5
+
 
 kill $SERVER_PID
 
